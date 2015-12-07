9b80185c757c719b697b4d0144d17cde30e74079
karolmuszynski <karol.muszynski@vp.pl>
2015-12-07 13:27:15 +0100

    Uploading final version

---
+++ ComputerGraphicsLab03/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace ComputerGraphicsLab03
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textbox_chooseSize = new System.Windows.Forms.TextBox();
+            this.numericUD_size = new System.Windows.Forms.NumericUpDown();
+            this.pictureBox_image = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.btn_chooseColor = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.btn_deleteLine = new System.Windows.Forms.Button();
+            this.check_circle = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUD_size)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_image)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textbox_chooseSize
+            // 
+            this.textbox_chooseSize.Enabled = false;
+            this.textbox_chooseSize.Location = new System.Drawing.Point(9, 58);
+            this.textbox_chooseSize.Margin = new System.Windows.Forms.Padding(2);
+            this.textbox_chooseSize.Multiline = true;
+            this.textbox_chooseSize.Name = "textbox_chooseSize";
+            this.textbox_chooseSize.ReadOnly = true;
+            this.textbox_chooseSize.Size = new System.Drawing.Size(84, 20);
+            this.textbox_chooseSize.TabIndex = 1;
+            this.textbox_chooseSize.Text = "Choose size:";
+            this.textbox_chooseSize.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // numericUD_size
+            // 
+            this.numericUD_size.Location = new System.Drawing.Point(9, 83);
+            this.numericUD_size.Margin = new System.Windows.Forms.Padding(2);
+            this.numericUD_size.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUD_size.Name = "numericUD_size";
+            this.numericUD_size.Size = new System.Drawing.Size(82, 20);
+            this.numericUD_size.TabIndex = 2;
+            this.numericUD_size.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.numericUD_size.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUD_size.ValueChanged += new System.EventHandler(this.numericUD_size_ValueChanged);
+            // 
+            // pictureBox_image
+            // 
+            this.pictureBox_image.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox_image.Location = new System.Drawing.Point(114, 10);
+            this.pictureBox_image.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox_image.Name = "pictureBox_image";
+            this.pictureBox_image.Size = new System.Drawing.Size(526, 570);
+            this.pictureBox_image.TabIndex = 3;
+            this.pictureBox_image.TabStop = false;
+            this.pictureBox_image.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.BlueViolet;
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox2.Location = new System.Drawing.Point(9, 34);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(84, 20);
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // btn_chooseColor
+            // 
+            this.btn_chooseColor.Location = new System.Drawing.Point(9, 10);
+            this.btn_chooseColor.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_chooseColor.Name = "btn_chooseColor";
+            this.btn_chooseColor.Size = new System.Drawing.Size(82, 20);
+            this.btn_chooseColor.TabIndex = 6;
+            this.btn_chooseColor.Text = "Choose color";
+            this.btn_chooseColor.UseVisualStyleBackColor = true;
+            this.btn_chooseColor.Click += new System.EventHandler(this.btn_chooseColor_Click);
+            // 
+            // btn_deleteLine
+            // 
+            this.btn_deleteLine.Location = new System.Drawing.Point(9, 106);
+            this.btn_deleteLine.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_deleteLine.Name = "btn_deleteLine";
+            this.btn_deleteLine.Size = new System.Drawing.Size(82, 20);
+            this.btn_deleteLine.TabIndex = 7;
+            this.btn_deleteLine.Text = "Delete line";
+            this.btn_deleteLine.UseVisualStyleBackColor = true;
+            this.btn_deleteLine.Click += new System.EventHandler(this.btn_deleteLine_Click);
+            // 
+            // check_circle
+            // 
+            this.check_circle.AutoSize = true;
+            this.check_circle.Location = new System.Drawing.Point(13, 132);
+            this.check_circle.Name = "check_circle";
+            this.check_circle.Size = new System.Drawing.Size(80, 17);
+            this.check_circle.TabIndex = 8;
+            this.check_circle.Text = "Draw Circle";
+            this.check_circle.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(644, 582);
+            this.Controls.Add(this.check_circle);
+            this.Controls.Add(this.btn_deleteLine);
+            this.Controls.Add(this.btn_chooseColor);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox_image);
+            this.Controls.Add(this.numericUD_size);
+            this.Controls.Add(this.textbox_chooseSize);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUD_size)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox_image)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textbox_chooseSize;
+        private System.Windows.Forms.NumericUpDown numericUD_size;
+        private System.Windows.Forms.PictureBox pictureBox_image;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button btn_chooseColor;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button btn_deleteLine;
+        private System.Windows.Forms.CheckBox check_circle;
+    }
+}
+
---
+++ ComputerGraphicsLab03/Form1.cs
@@ -0,0 +1,387 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ComputerGraphicsLab03
+{
+    public partial class Form1 : Form
+    {
+        List<Line> myLines = new List<Line>();
+        Line _current = new Line();
+        Bitmap bitmap;
+        bool changeStartPoint;
+        bool changeEndPoint;
+        bool isDrawing;
+        bool deleteLine;
+        int index;
+        int pixel =1;
+        private Point _startPoint = Point.Empty;
+        private Point _endPoint = Point.Empty;
+
+        public class Line
+        {
+            public Point startPoint;
+            public Point endPoint;
+            public Color myColor;
+            public int size;
+            
+            
+            public Line(Point _startPoint, Point _endPoint, Color _myColor, int _size)
+            {
+                this.startPoint = _startPoint;
+                this.endPoint = _endPoint;
+                this.myColor = _myColor;
+                this.size = _size;
+            }
+
+            public Line()
+            {
+            }
+        }
+
+        public Form1()
+        {
+            changeStartPoint = false;
+            changeEndPoint = false;
+            isDrawing = false;
+            deleteLine = false;
+            InitializeComponent();
+            _current.startPoint = new Point(-1, -1);
+            bitmap = new Bitmap(pictureBox_image.Width, pictureBox_image.Height);
+            pictureBox_image.Image = bitmap;
+            colorDialog1.Color = Color.BlueViolet;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void btn_chooseColor_Click(object sender, EventArgs e)
+        {
+            DialogResult result = colorDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+                pictureBox2.BackColor = colorDialog1.Color;
+        }
+
+
+
+        void draw(Line line)
+        {
+            
+         
+            int d, dx, dy, ai, bi, xi, yi;
+            int x = line.startPoint.X, y = line.startPoint.Y;
+            int x1 = line.startPoint.X;
+            int x2 = line.endPoint.X;
+            int y1 = line.startPoint.Y;
+            int y2 = line.endPoint.Y;
+            int ddx = 0;
+            int ddy = 0;
+            // set drawing direction
+            if (x1 < x2)
+            {
+                xi = 1;
+                dx = line.endPoint.X - line.startPoint.X;
+            }
+            else
+            {
+                xi = -1;
+                dx = line.startPoint.X - line.endPoint.X;
+            }
+            // set drawing direction
+            if (y1 < y2)
+            {
+                yi = 1;
+                dy = line.endPoint.Y - line.startPoint.Y;
+            }
+            else
+            {
+                yi = -1;
+                dy = line.startPoint.Y - line.endPoint.Y;
+            }
+            // first pixel
+            bitmap.SetPixel(x, y, line.myColor);
+            // main OX
+            if (dx > dy)
+            {
+                ai = (dy - dx) * 2;
+                bi = dy * 2;
+                d = bi - dx;
+                // for next x's
+                while (x != x2)
+                {
+                    // test coefficent
+                    if (d >= 0)
+                    {
+                        x += xi;
+                        y += yi;
+                        d += ai;
+                        ddy = line.size;
+
+                    }
+                    else
+                    {
+                        d += bi;
+                        x += xi;
+                        ddy = line.size;
+
+                    }
+                    bitmap.SetPixel(x, y, line.myColor);
+                    if (ddy > 1)
+                    {
+
+                        for (int i = 0; i < ddy; i++)
+                        {
+                            if (line.startPoint.Y + i < 700)
+                                bitmap.SetPixel(line.startPoint.X, line.startPoint.Y + i, line.myColor);
+                            if (line.startPoint.Y - i >= 0)
+                                bitmap.SetPixel(line.startPoint.X, line.startPoint.Y - i, line.myColor);
+                            if (y + i < 700)
+                                bitmap.SetPixel(x, y + i, line.myColor);
+                            if (y - i >= 0)
+                                bitmap.SetPixel(x, y - i, line.myColor);
+                        }
+                    }
+                }
+            }
+            // main OY
+            else
+            {
+                ai = (dx - dy) * 2;
+                bi = dx * 2;
+                d = bi - dy;
+                //  for next y's
+                while (y != y2)
+                {
+                    // test coefficent
+                    if (d >= 0)
+                    {
+                        x += xi;
+                        y += yi;
+                        d += ai;
+                        ddx = line.size;
+
+                    }
+                    else
+                    {
+                        d += bi;
+                        y += yi;
+                        ddx = line.size;
+
+                    }
+                    bitmap.SetPixel(x, y, line.myColor);
+                    if (ddx > 1)
+                    {
+
+                        for (int i = 0; i < ddx; i++)
+                        {
+                            if (line.startPoint.X + i < 700)
+                                bitmap.SetPixel(line.startPoint.X + i, line.startPoint.Y, line.myColor);
+                            if (line.startPoint.X - i >= 0)
+                                bitmap.SetPixel(line.startPoint.X - i, line.startPoint.Y, line.myColor);
+                            if (x + i < 700)
+                                bitmap.SetPixel(x + i, y, line.myColor);
+                            if (x - i >= 0)
+                                bitmap.SetPixel(x - i, y, line.myColor);
+                        }
+                    }
+
+                }
+            }
+            pictureBox_image.Refresh();
+
+        }
+
+
+        private bool firstPoint(Line l)
+        {
+            if (l.startPoint.Equals(new Point(-1, -1)))
+                return true;
+            else
+                return false;
+        }
+
+        void PutPixel(Graphics g, int x, int y, Color c, int p)
+        {
+            //p = pixel;
+            Bitmap bm = new Bitmap(1,1);
+            bm.SetPixel(0, 0, c);
+            var result = new Bitmap(bm,p,p);
+            g.DrawImageUnscaled(result, x, y);
+        }
+
+        private void DrawCircle(int x1, int y1, int x2, int y2)
+        {
+            Graphics g = Graphics.FromImage(pictureBox_image.Image);
+
+            int R;
+            double xd = x2 - x1;
+            double yd = y2 - y1;
+            R = Convert.ToInt32(Math.Sqrt((xd * xd) + (yd * yd)));
+
+            int x = R, y = 0;//local coords     
+            int cd2 = 0;    //current distance squared - radius squared
+
+            PutPixel(g, x1 - R, y1, colorDialog1.Color, pixel);
+            PutPixel(g, x1 + R, y1, colorDialog1.Color, pixel);
+            PutPixel(g, x1, y1 - R, colorDialog1.Color, pixel);
+            PutPixel(g, x1, y1 + R, colorDialog1.Color, pixel);
+
+            while (x > y)
+            {    //only formulate 1/8 of circle
+                cd2 -= (--x) - (++y);
+                if (cd2 < 0) cd2 += x++;
+                //upper left left
+                PutPixel(g, x1 - x, y1 - y, colorDialog1.Color, pixel);
+                //upper upper left
+                PutPixel(g, x1 - y, y1 - x, colorDialog1.Color, pixel);
+                //upper upper right
+                PutPixel(g, x1 + y, y1 - x, colorDialog1.Color, pixel);
+                //upper right right
+                PutPixel(g, x1 + x, y1 - y, colorDialog1.Color, pixel);
+                //lower left left
+                PutPixel(g, x1 - x, y1 + y, colorDialog1.Color, pixel);
+                //lower lower left
+                PutPixel(g, x1 - y, y1 + x, colorDialog1.Color, pixel);
+                //lower lower right
+                PutPixel(g, x1 + y, y1 + x, colorDialog1.Color, pixel);
+                //lower right right
+                PutPixel(g, x1 + x, y1 + y, colorDialog1.Color, pixel);
+            }
+            g.Dispose();
+            this.pictureBox_image.Refresh();
+            _current = new Line();
+            _current.startPoint = new Point(-1, -1);
+            isDrawing = false;
+            changeEndPoint = false;
+            changeStartPoint = false;
+        }
+
+        private void redrawLines()
+        {
+            bitmap = new Bitmap(pictureBox_image.Width, pictureBox_image.Height);
+            pictureBox_image.Image = bitmap;
+
+            foreach(Line myLine in myLines)
+                draw(myLine);
+
+            pictureBox_image.Refresh();
+            return;
+        }
+
+        private bool endpointNear(Point _location)
+        {
+            for (int i = 0; i < myLines.Count; i++)
+            {
+                if ((Math.Abs(myLines[i].endPoint.X - _location.X) < 15 && Math.Abs(myLines[i].endPoint.Y - _location.Y) < 15))
+                {
+                    index = i;
+                    if(!deleteLine)
+                        changeEndPoint = true;
+                    return true;
+                }
+                else if((Math.Abs(myLines[i].startPoint.X - _location.X) < 15 && Math.Abs(myLines[i].startPoint.Y - _location.Y) < 15))
+                {
+                    index = i;
+                    if (!deleteLine)
+                        changeStartPoint = true;
+                    return true;
+                }
+            }
+         return false;
+        }
+
+        private void pictureBox1_MouseClick(object sendPointer, MouseEventArgs e)
+        {
+            if (isDrawing)
+            {
+                if (deleteLine)
+                {
+                    deleteLine = false;
+                    return;
+                }
+
+                _current.endPoint = e.Location;
+                _current.myColor = colorDialog1.Color;
+                _current.size = (int)numericUD_size.Value;
+                if (check_circle.Checked==true)
+                {
+                    this.DrawCircle(_current.startPoint.X, _current.startPoint.Y, _current.endPoint.X, _current.endPoint.Y);
+                    _startPoint = Point.Empty;
+                    _endPoint = Point.Empty;
+                }
+                else
+                {
+                    draw(_current);
+                    myLines.Add(new Line(_current.startPoint, _current.endPoint, colorDialog1.Color, _current.size));
+                    _current.startPoint = new Point(-1, -1);
+                    isDrawing = false;
+                    return;
+                }
+            }
+            else 
+            {
+                if (changeEndPoint)
+                {
+                    myLines[index].endPoint = new Point(e.Location.X, e.Location.Y);
+                    changeEndPoint = false;
+                    redrawLines();
+                    return;
+                }
+                else if (changeStartPoint)
+                {
+                    myLines[index].startPoint = new Point(e.Location.X, e.Location.Y);
+                    changeStartPoint = false;
+                    redrawLines();
+                    return;
+                }
+
+                if (!endpointNear(e.Location))
+                {
+                    if (deleteLine)
+                    {
+                        deleteLine = false;
+                        return;
+                    }
+
+                    if (firstPoint(_current))
+                    {
+                        _current.startPoint = e.Location;
+                        isDrawing = true;
+                        return;
+                    }
+                }
+                else
+                {
+                    if (deleteLine)
+                    {
+                        myLines.RemoveAt(index);
+                        redrawLines();
+                        deleteLine = false;
+                        return;
+                    }
+                }
+            } 
+        }
+
+        private void btn_deleteLine_Click(object sender, EventArgs e)
+        {
+            if (myLines.Count > 0)
+            {
+                deleteLine = true;
+            }
+        }
+
+        private void numericUD_size_ValueChanged(object sender, EventArgs e)
+        {
+            pixel = (int)numericUD_size.Value;
+        } 
+    }
+}
